80e0c3647023224307622591489feca59197cde0
sanziowesley <sanziowesley@hotmail.com>
2021-04-29 21:12:31 -0300

    Cadastro de usuário - Bloquear inclusão de usuários já cadastrados parte 1

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,19 +1,58 @@
 <?php
 
-require_once 'db.class.php';
+    require_once 'db.class.php';
 
-$usuario = $_POST['usuario'];
-$email = $_POST['email'];
-$senha = md5($_POST['senha']);
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $senha = md5($_POST['senha']);
 
-$objDb = new db();
-$link = $objDb->conecta_mysql();
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
 
-$sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
+    //verificar se o usuário já existe
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' ";
+    if ($resultado_id = mysqli_query($link, $sql)) {
 
-//executar a query
-if (mysqli_query($link, $sql)) {
-    echo "Usuário registrado com sucesso!";
-} else {
-    echo "Erro ao registrar o usuário!";
-}
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if (isset($dados_usuario['usuario'])) {
+
+            echo 'Usuário já cadastrado';
+
+        } else {
+            echo ' Usuário não cadastrado, ok, pode cadastrar ';
+        }
+
+    } else {
+        echo ' Erro ao tentar localizar o registro de usuário ';
+    }
+
+    //verificar se o e-mail já existe
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' ";
+    if ($resultado_id = mysqli_query($link, $sql)) {
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if (isset($dados_usuario['email'])) {
+            echo ' Email já cadastrado ';
+
+        } else {
+            echo 'Email não cadastrado, ok, pode cadastrar';
+        }
+
+    } else {
+        echo 'Erro ao tentar localizar o registro de email';
+    }
+
+    die();
+
+    $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
+
+    //executar a query
+    if (mysqli_query($link, $sql)) {
+        echo "Usuário registrado com sucesso!";
+    } else {
+        echo "Erro ao registrar o usuário!";
+    }
+
+?>
