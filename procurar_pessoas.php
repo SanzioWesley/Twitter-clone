0db4074b066ba96b34858dfb0792df3bbcba7eab
sanziowesley <sanziowesley@hotmail.com>
2021-05-02 21:41:58 -0300

    Deixando de seguir pessoas

--- procurar_pessoas.php
+++ procurar_pessoas.php
@@ -10,12 +10,9 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="UTF-8">
-
 		<title>Twitter clone</title>
-
 		<!-- jquery - link cdn -->
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-
 		<!-- bootstrap - link cdn -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
@@ -28,33 +25,35 @@
 					if($('#nome_pessoa').val().length > 0){
 
 						$.ajax({
-
 							url: 'get_pessoas.php',
-
 							method: 'POST',
-
 							data: $('#form_procurar_pessoas').serialize(),
-
 							success: function(data){
-
 								$('#pessoas').html(data);
-
 								$('.btn_seguir').click(function(){
+									var id_usuario = $(this).data('id_usuario');
+							
+									$.ajax({
+										url: 'seguir.php',
+										method: 'POST',
+										data: { seguir_id_usuario : id_usuario },
+										success: function(data){
+											alert("Registro efetuado com sucesso!");
+										}
+									});
+
+								});
+
+								$('.btn_deixar_seguir').click(function(){
 
 									var id_usuario = $(this).data('id_usuario');
 
 									$.ajax({
-
-										url: 'seguir.php',
-
+										url: 'deixar_seguir.php',
 										method: 'POST',
-
-										data: { seguir_id_usuario : id_usuario },
-
+										data: { deixar_seguir_id_usuario : id_usuario },
 										success: function(data){
-
-											alert("Registro efetuado com sucesso!");
-
+											alert("Registro removido com sucesso!");
 										}
 
 									});
